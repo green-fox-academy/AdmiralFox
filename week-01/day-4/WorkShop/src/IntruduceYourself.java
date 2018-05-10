61a82971476d7f7ab85d330a34fdb2a21f09e6a8
SzalontaiKrisztian <krisztian.szalontai@outlook.com>
2018-05-10 13:29:01 +0200

    IntroduceYourself task

---
+++ week-01/day-4/WorkShop/src/IntruduceYourself.java
@@ -0,0 +1,7 @@
+public class IntruduceYourself {
+    public static void main(String[] args) {
+        System.out.println("Krisztian Szalontai");
+        System.out.println("30");
+        System.out.println(1.82);
+    }
+}
