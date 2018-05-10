01113eff3a1830327d225135f7b97e17edc6a9cf
SzalontaiKrisztian <krisztian.szalontai@outlook.com>
2018-05-10 13:54:27 +0200

    TwoNumbers task

---
+++ week-01/day-4/WorkShop/src/TwoNumbers.java
@@ -0,0 +1,11 @@
+public class TwoNumbers {
+    public static void main(String[] args) {
+        System.out.println(13 + 22);
+        System.out.println(22 - 13);
+        System.out.println(22 * 13);
+        System.out.println(22. / 13.);
+        int a = (22 / 13);
+        System.out.println(a);
+        System.out.println(22 % 13);
+    }
+}
