caa13f46cedecd3732855b56edb3eae7f727f125
SzalontaiKrisztian <krisztian.szalontai@outlook.com>
2018-05-10 17:12:44 +0200

    DefineBasicInfo task

---
+++ week-01/day-4/WorkShop/src/DefineBasicInfo.java
@@ -0,0 +1,12 @@
+public class DefineBasicInfo {
+    public static void main(String[] args) {
+        String name = "AdmiralFox";
+        System.out.println(name);
+        int age = 30;
+        System.out.println(age);
+        double height = 1.82;
+        System.out.println(height);
+        boolean married = true;
+        System.out.println(married);
+        }
+    }
