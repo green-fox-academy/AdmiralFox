7f2d29b9dad21cc90c3add10e7a78ed907602c60
SzalontaiKrisztian <krisztian.szalontai@outlook.com>
2018-05-10 13:20:44 +0200

    HelloOthers task

---
+++ week-01/day-4/WorkShop/src/HelloOthers.java
@@ -0,0 +1,7 @@
+public class HelloOthers {
+    public static void main(String[] args) {
+        System.out.println("Hello Lilla");
+        System.out.println("Hello Peter");
+        System.out.println("Hello Krisztian");
+    }
+}
