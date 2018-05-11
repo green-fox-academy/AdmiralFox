c86be60f96d14aee563759840d067da394722f7f
SzalontaiKrisztian <krisztian.szalontai@outlook.com>
2018-05-11 12:48:16 +0200

    OddEven task

---
+++ week-01/day-4/WorkShop/src/OddEven.java
@@ -0,0 +1,10 @@
+public class OddEven {
+    public static void main(String[] args) {
+        int a = 17;
+        if (a % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+}
