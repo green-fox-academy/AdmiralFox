6c8ba5638f0ce75e8538869eebed167f4b57e327
SzalontaiKrisztian <krisztian.szalontai@outlook.com>
2018-05-11 12:55:34 +0200

    OneTwoALot task

---
+++ week-01/day-4/WorkShop/src/OneTwoALot.java
@@ -0,0 +1,14 @@
+public class OneTwoALot {
+    public static void main(String[] args) {
+        int a = -3;
+        if (a <= 0) {
+            System.out.println("Not enogh");
+        } else if (a == 1) {
+            System.out.println("One");
+        } else if (a == 2) {
+            System.out.println("Two");
+        }else {
+            System.out.println("A lot");
+        }
+    }
+}
