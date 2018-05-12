83f550df9cf6b58c1d51b0f603fbd48d07920e20
SzalontaiKrisztian <krisztian.szalontai@outlook.com>
2018-05-12 15:29:40 +0200

    DrawTriangle & FizzBuzz task

---
+++ week-01/day-4/WorkShop/src/DrawTriangle.java
@@ -0,0 +1,18 @@
+public class DrawTriangle {
+    public static void main(String[] args) {
+
+        int triangle = 4;
+
+        for (int a = 0; a < triangle; a++) {
+            for (int b = 0; b < triangle; b++) {
+                if (b <= a) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
