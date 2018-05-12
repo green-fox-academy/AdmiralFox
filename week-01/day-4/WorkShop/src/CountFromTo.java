6dabfcb9d906c3766a6ecb5a9ab4e0cd9b241a26
SzalontaiKrisztian <krisztian.szalontai@outlook.com>
2018-05-12 14:22:13 +0200

    CountFromTo task

---
+++ week-01/day-4/WorkShop/src/CountFromTo.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class CountFromTo {
+    public static void main(String[] args) {
+ 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please add zour first number here: ");
+        int firstNumber = scanner.nextInt();
+        System.out.println("Please add zour second number here: ");
+        int secondNumber = scanner.nextInt();
+        if (secondNumber < firstNumber) {
+            System.out.println("The second number should be bigger");
+        } else {
+            for (int a = firstNumber; a < secondNumber; a++) {
+                System.out.println(a);
+            }
+        }
+
+    }
+}
