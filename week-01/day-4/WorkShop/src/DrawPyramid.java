a8f0c0b37bff70a2c2987943d878ffd6e18c9b01
SzalontaiKrisztian <krisztian.szalontai@outlook.com>
2018-05-13 12:41:33 +0200

    DrawPyramid task

--- week-01/day-4/WorkShop/src/DrawPyramid.java
+++ week-01/day-4/WorkShop/src/DrawPyramid.java
@@ -1,15 +1,5 @@
 public class DrawPyramid {
     public static void main(String[] args) {
-        // Write a program that reads a number from the standard input, then draws a
-        // pyramid like this:
-        //
-        //
-        //    *
-        //   ***
-        //  *****
-        // *******
-        //
-        // The pyramid should have as many lines as the number was
         for (int i = 1; i <= 4; i++)
         {
             for (int j = i; j < 4; j++)
