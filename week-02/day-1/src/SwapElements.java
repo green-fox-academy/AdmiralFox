1d876fdb152ca6c3581cf3b3f41ba7e6c8ae77ff
SzalontaiKrisztian <krisztian.szalontai@outlook.com>
2018-05-14 17:46:46 +0200

    SwapElements task not finished

---
+++ week-02/day-1/src/SwapElements.java
@@ -0,0 +1,15 @@
+public class SwapElements {
+    public static void main(String[] args) {
+        // - Create an array variable named `abc`
+        //   with the following content: `["first", "second", "third"]`
+        // - Swap the first and the third element of `abc`
+
+        String[] abc = {"first", "second", "third"};
+      
+
+        if (abc[0] == "first" && abc[2] == "third");
+            abc[0] = "abc";
+            abc[2] = "abc";
+
+         }
+    }
