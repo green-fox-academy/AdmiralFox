bcc04f4b97061e7cd2baccbe1b61ed68c090a963
SzalontaiKrisztian <krisztian.szalontai@outlook.com>
2018-05-16 15:55:48 +0200

    WriteMultipleLines task

--- week-01/day-4/src/Test.java
+++ week-01/day-4/src/Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-        for (int a = 1; a < 5; a++) {
+        for (int a = 0; a < 5; a++) {
             for (int b = 5; b > a; b--) {
                 System.out.print(" ");
             }
