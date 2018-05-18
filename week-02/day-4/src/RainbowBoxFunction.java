5454d39560bd766d50f6cdb76788129ea4b057e7
SzalontaiKrisztian <krisztian.szalontai@outlook.com>
2018-05-18 16:52:46 +0200

    RainbowBoxFunction task

--- week-02/day-4/src/RainbowBoxFunction.java
+++ week-02/day-4/src/RainbowBoxFunction.java
@@ -12,28 +12,26 @@ public class RainbowBoxFunction {
         // The square size, and the fill color,
         // and draws a square of that size and color to the center of the canvas.
         // Create a loop that fills the canvas with rainbow colored squares.
-        int sizex = 10;
-        int sizey = 10;
-        centerSquare(sizex, sizey, graphics);
-       /*
-        This lines help me to centralized the boxes:
-        graphics.setColor(Color.BLACK);
-        graphics.drawRect(0,0,300,300);
-        graphics.drawLine(0, 150, 300, 150);
-        graphics.drawLine(150, 0, 150, 300);
-        */
+        int sizeOfSquare = 10;
+        centerSquare(sizeOfSquare, graphics);
     }
-    public static void centerSquare ( int sizex, int sizey, Graphics graphics) {
+    public static void centerSquare ( int size, Graphics graphics) {
         for (int i = WIDTH; i > 0; i--) {
+            int canvasSize = WIDTH / 2;
             Random random = new Random();
-            graphics.setColor(new Color(random.nextInt(256),random.nextInt(256),random.nextInt(256)));
-            graphics.fillRect(150 - (sizex * i / 2) , 150 - (sizey * i / 2), sizex * i , sizey * i);
+            graphics.setColor(new Color(random.nextInt(256),
+                                        random.nextInt(256),
+                                        random.nextInt(256)));
+            graphics.fillRect(   canvasSize - (size * i / 2) ,
+                                 canvasSize - (size * i / 2),
+                                 size * i,
+                                 size * i);
         }
     }
 
     //    Don't touch the code below
-    static int WIDTH = 300;
-    static int HEIGHT = 323;
+    static int WIDTH = 600;
+    static int HEIGHT = 623;
 
     public static void main(String[] args) {
         JFrame jFrame = new JFrame("Drawing");
