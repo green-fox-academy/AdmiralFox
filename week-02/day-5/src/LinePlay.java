87514d0f21885b6a31750836f7c11f54cdf2e8aa
SzalontaiKrisztian <krisztian.szalontai@outlook.com>
2018-05-19 15:53:35 +0200

    LinePlay half finished

---
+++ week-02/day-5/src/LinePlay.java
@@ -0,0 +1,41 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class LinePlay {
+    public static void mainDraw(Graphics graphics) {
+        lines(graphics);
+
+
+
+    }
+    public static void lines (Graphics graphics) {
+        for (int i = 0; i < WIDTH; i += 20) {
+            graphics.setColor(new Color(166,102,234));
+            graphics.drawLine(20 + i, 5, WIDTH, 20 + i);
+        }
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 300;
+    static int HEIGHT = 323;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        jFrame.add(new ImagePanel());
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
