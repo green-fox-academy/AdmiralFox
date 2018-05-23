0ac2534b964881ed3eb5318ec90678497ddb842f
SzalontaiKrisztian <krisztian.szalontai@outlook.com>
2018-05-23 15:08:00 +0200

    Domino task

---
+++ week-03/day-3/src/Trees.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class Trees {
+    public static void main(String[] args) {
+        Object[] palmTree = {"Palmtree","Green", 25, "female"};
+        System.out.println(Arrays.toString(palmTree));
+        Object[] Oak = {"Oak","Green", 38, "male"};
+        System.out.println(Arrays.toString(Oak));
+        Object[] OliveTree = {"Oak","Green", 38, "male"};
+        System.out.println(Arrays.toString(OliveTree));
+        Object[] Cypress = {"Oak","Green", 18, "female"};
+        System.out.println(Arrays.toString(Cypress));
+        Object[] Willow = {"Oak","Green", 75, "male"};
+        System.out.println(Arrays.toString(Willow));
+    }
+}
