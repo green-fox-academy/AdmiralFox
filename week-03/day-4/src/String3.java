f4338cba6afa313a5647ee3194b0468524153957
SzalontaiKrisztian <krisztian.szalontai@outlook.com>
2018-05-24 16:46:29 +0200

    String3 task not ready

--- week-03/day-4/src/String3.java
+++ week-03/day-4/src/String3.java
@@ -3,11 +3,10 @@ public class String3 {
         // Given a string, compute recursively (no loops) a new string where all the
         // lowercase 'x' chars have been changed to 'y' chars.
         String sentence = "xxx films ar the most downloaded films at the xxx.com sites";
-        System.out.println(starChanger(sentence);
+        //System.out.println(starChanger(sentence);
     }
 
-    private static String starChanger(String sentence) {
-        
+   // private static String starChanger(String sentence) {
+
 
     }
-}
