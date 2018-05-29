f583aa0c24f88cf4a2ba9c2a2eba8d1d1b3aa655
SzalontaiKrisztian <krisztian.szalontai@outlook.com>
2018-05-29 15:56:15 +0200

    Cohort rethinking

--- week-04/day-2/src/Cohort.java
+++ week-04/day-2/src/Cohort.java
@@ -3,54 +3,24 @@ import java.util.List;
 
 public class Cohort {
 
-    public String name;
-    public List<Student> students;
-    public List<Mentor> mentors;
+    String name;
+    List<Student> students = new ArrayList<>();
+    List<Mentor>  mentors = new ArrayList<>();
 
-    public static void main(String[] args) {
+    public Cohort(String name) {
+        this.name = name;
 
-        ArrayList<Person> people = new ArrayList<>();
-
-        Person mark = new Person("Mark", 46, "male");
-        people.add(mark);
-        Person jane = new Person();
-        people.add(jane);
-        Student john = new Student("John Doe", 20, "male", "BME");
-        people.add(john);
-        Student student = new Student();
-        people.add(student);
-        Mentor gandhi = new Mentor("Gandhi", 148, "male", "senior");
-        people.add(gandhi);
-        Mentor mentor = new Mentor();
-        people.add(mentor);
-        Sponsor sponsor = new Sponsor();
-        people.add(sponsor);
-        Sponsor elon = new Sponsor("Elon Musk", 46, "male", "SpaceX");
-        people.add(elon);
-
-        student.skipDays(3);
-
-        for (int i = 0; i < 5; i++) {
-            elon.hire();
-        }
-
-        for (int i = 0; i < 3; i++) {
-            sponsor.hire();
-        }
-
-        for(Person person : people) {
-            person.introduce();
-            person.getGoal();
-        }
-
-
-
-//        Cohort awesome = new Cohort("AWESOME");
-//        awesome.addStudent(student);
-//        awesome.addStudent(john);
-//        awesome.addMentor(mentor);
-//        awesome.addMentor(gandhi);
-//        awesome.info();
     }
 
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
+    public void addMentor(Mentor mentor) {
+        mentors.add(mentor);
+    }
+
+    public void info() {
+        System.out.println("The " +  name + " cohort has " + students.size() + " students " + "and " + mentors.size() + " mentors " + ".");
+    }
 }
