b1f7372b9cd3c31293617464aebe1ecfafaa5cbd
SzalontaiKrisztian <krisztian.szalontai@outlook.com>
2018-05-29 16:40:37 +0200

    feat(Plant) field added

---
+++ week-04/day-2/gardenApplication/src/Plant.java
@@ -0,0 +1,26 @@
+public class Plant {
+
+    private String color;
+    private int waterLevel;
+
+    public Plant() {
+        this.color = color;
+        this.waterLevel = waterLevel;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getWaterLevel() {
+        return waterLevel;
+    }
+
+    public void setWaterLevel(int waterLevel) {
+        this.waterLevel = waterLevel;
+    }
+}
