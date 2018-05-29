5ab5889ac17d9110ac7e986be54d3df3d4b914e9
SzalontaiKrisztian <krisztian.szalontai@outlook.com>
2018-05-29 12:56:57 +0200

    Student class main functions ready

---
+++ week-04/day-2/src/Student.java
@@ -0,0 +1,29 @@
+public class Student extends Person {
+
+    private String previousOrganization;
+    private int skippedDays;
+
+
+
+    public void getGoal() {
+        System.out.println("Be a junior software developer");
+    }
+    public void introduce() {
+        System.out.println("Hi, I`am" +
+                this.name +",a " +
+                this.age + "year old " +
+                this.gender + "from" +
+                this.previousOrganization + " who skipped" +
+                this.skippedDays + " days from the course already." );
+    }
+
+    public int skipDays(numberOfDays) {
+        
+    }
+
+    public Student(String name, int age, String gender, String previousOrganization) {
+        super(name, age, gender);
+        this.previousOrganization = "The School of Life";
+        this.skippedDays = 0;
+    }
+}
