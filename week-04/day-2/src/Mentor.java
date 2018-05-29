428860035387039611ccd4be091bca53cc84c3b7
SzalontaiKrisztian <krisztian.szalontai@outlook.com>
2018-05-29 14:12:57 +0200

    Mentor task ready and working

--- week-04/day-2/src/Mentor.java
+++ week-04/day-2/src/Mentor.java
@@ -6,11 +6,15 @@ public class Mentor extends Person {
         System.out.println("Educate brilliant junior software developers.");
     }
     public void introduce() {
-        System.out.println("Hi, I'm" + this.name+", a " + this.age + " year old " + this.gender + this.level + "mentor.");
+        System.out.println("Hi, I'm " + this.name+", a " + this.age + " year old " + this.gender + this.level + " mentor.");
+    }
+
+    public Mentor() {
+        super();
+        this.level = " intermediate";
     }
 
     public Mentor(String name, int age, String gender, String level) {
         super(name, age, gender);
-        this.level = "intermediate";
     }
 }
