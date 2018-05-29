ccb359d67461da3f4a72a77cce344e4f807eee9e
SzalontaiKrisztian <krisztian.szalontai@outlook.com>
2018-05-29 13:07:02 +0200

    Mentor class ready to use

---
+++ week-04/day-2/src/Mentor.java
@@ -0,0 +1,16 @@
+public class Mentor extends Person {
+
+    public String level;
+
+    public void getGoal() {
+        System.out.println("Educate brilliant junior software developers.");
+    }
+    public void introduce() {
+        System.out.println("Hi, I'm" + this.name+", a " + this.age + " year old " + this.gender + this.level + "mentor.");
+    }
+
+    public Mentor(String name, int age, String gender, String level) {
+        super(name, age, gender);
+        this.level = "intermediate";
+    }
+}
