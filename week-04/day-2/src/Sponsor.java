66c180597488c03fee2cdc9a67ddb802e789c9a3
SzalontaiKrisztian <krisztian.szalontai@outlook.com>
2018-05-29 13:17:22 +0200

    Sponsor class ready to use

---
+++ week-04/day-2/src/Sponsor.java
@@ -0,0 +1,27 @@
+public class Sponsor extends Person{
+
+    public String company;
+    public int hiredStudents;
+
+    public void introduce() {
+        System.out.println("Hi, I'm" +
+                this.name + ", a " +
+                this.age + " year old " +
+                this.gender + " who represents " +
+                this.company + " and hired" +
+                this.hiredStudents + "students so far.");
+    }
+    public void hire() {
+        hiredStudents++;
+    }
+    public void getGoal() {
+        System.out.println("Hire brilliant junior software developers.");
+    }
+
+    public Sponsor(String name, int age, String gender, String company) {
+        super(name, age, gender);
+        this.company = "Google";
+        this.hiredStudents = 0;
+    }
+}
+
