e9cf2d887516876f8e81845973193d85034e3e14
SzalontaiKrisztian <krisztian.szalontai@outlook.com>
2018-05-29 17:23:50 +0200

    feat(Tree) class created

---
+++ week-04/day-2/gardenApplication/src/Tree.java
@@ -0,0 +1,13 @@
+public class Tree extends Plant {
+
+  public Tree(String name, String color, int waterLevel, boolean needsOrNot) {
+    this.name = "Tree";
+    this.color = "purle";
+    this.waterLevel = 0;
+    this.boolean = waterLevel < 5;
+
+  }
+}
+
+}
+
