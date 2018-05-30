8935faa137433fe9de80e14748b7b690624f9243
SzalontaiKrisztian <krisztian.szalontai@outlook.com>
2018-05-30 13:42:31 +0200

    feat(Dominoes) tested reslults

--- week-03/day-3/Dominoes/src/Dominoes.java
+++ week-03/day-3/Dominoes/src/Dominoes.java
@@ -1,20 +1,24 @@
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class Dominoes {
     public static void main(String[] args) {
         List<Domino> dominoes = initializeDominoes();
         List<Domino> sortedDominoes = new ArrayList<>();
-        sortedDominoes.add(dominoes.get(0));
+//        sortedDominoes.add(dominoes.get(0));
+//        Domino domino1 = new Domino(8,6);
+//        Domino domino2 = new Domino(7,4);
+      System.out.println(dominoes);
 
-        for (int i = 0; i < (dominoes.size() - 1); i++) {
-            for (int j = 0; j < dominoes.size(); j++) {
-                if (sortedDominoes.get(i).getValues()[1] == dominoes.get(j).getValues()[0]) {
-                    sortedDominoes.add(dominoes.get(j));
-                }
-            }
-        }
-        System.out.println(sortedDominoes);
+//        for (int i = 0; i < (dominoes.size() - 1); i++) {
+//            for (int j = 0; j < dominoes.size(); j++) {
+//                if (sortedDominoes.get(i).getValues()[1] == dominoes.get(j).getValues()[0]) {
+//                    sortedDominoes.add(dominoes.get(j));
+//                }
+//            }
+//        }
     }
 
     static List<Domino> initializeDominoes() {
@@ -25,6 +29,13 @@ public class Dominoes {
         dominoes.add(new Domino(6, 7));
         dominoes.add(new Domino(2, 4));
         dominoes.add(new Domino(7, 1));
+        dominoes.add(new Domino(7, 5));
+        dominoes.add(new Domino(6, 2));
+
+
+        Collections.sort(dominoes);
+
         return dominoes;
     }
+
 }
