d446c365fb4c9ad9b4823be36a749d9f7f11506f
SzalontaiKrisztian <krisztian.szalontai@outlook.com>
2018-05-31 17:46:55 +0200

    Anagram after the test refactored

--- week-04/day-4/Anagram/src/Anagram.java
+++ week-04/day-4/Anagram/src/Anagram.java
@@ -5,25 +5,27 @@ public class Anagram {
   //  Write a function, that takes two strings and returns a boolean value based on if the two strings are Anagramms or not.
   //  Create a test for that.
   public static boolean isAnagram(String firstText, String secondText) {
-    ArrayList<String> compareOne = new ArrayList<>(Arrays.asList(firstText.split("")));
-    ArrayList<String> compareTwo = new ArrayList<>(Arrays.asList(secondText.split("")));
+    ArrayList<String> compareOne = new ArrayList<>(Arrays.asList(firstText.toLowerCase().split("")));
+    ArrayList<String> compareTwo = new ArrayList<>(Arrays.asList(secondText.toLowerCase().split("")));
 
+//    firstText.split("");
+//    System.out.println(Arrays.toString(firstText.split("")));
     compareOne.sort(String::compareTo);
     compareTwo.sort(String::compareTo);
 
 
     if (compareOne.toString().equals(compareTwo.toString())) {
-      System.out.println(compareOne.toString());
-      System.out.println(compareTwo.toString());
-      System.out.println("true");
       return true;
     }
     System.out.println("false");
     return false;
   }
+
   public static void main(String[] args) {
 
-    Anagram.isAnagram("cinema","iceman");
 
+    Anagram.isAnagram("aBd","aBd");
   }
+
+
 }
