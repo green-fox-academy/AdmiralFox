db5ade6c51850356e0be2ec652083c68aa10ef67
SzalontaiKrisztian <krisztian.szalontai@outlook.com>
2018-05-31 17:45:05 +0200

    AnagramTest Upper and Lower case test working

--- week-04/day-4/Anagram/test/AnagramTest.java
+++ week-04/day-4/Anagram/test/AnagramTest.java
@@ -11,15 +11,16 @@ public class AnagramTest {
     String testTextOne = "cinema";
     String testTextTwo = "iceman";
 
-    assertTrue(text.isAnagram(testTextOne,testTextTwo));
+    assertTrue(text.isAnagram(testTextOne, testTextTwo));
   }
+
   @Test
-  public void anagram_Test_with_Uppercase() {
+  public void anagram_Test_with_Upper_and_Lowercase() {
     Anagram text = new Anagram();
-    String testTextOne = "cinema";
-    String testTextTwo = "iceman";
+    String testTextOne = "ciNemA";
+    String testTextTwo = "IceMan";
 
-    assertTrue(text.isAnagram(testTextOne,testTextTwo));
-
+    assertTrue(text.isAnagram(testTextOne, testTextTwo));
+  }
 
 }
