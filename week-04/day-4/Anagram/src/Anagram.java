962a1fef2ed57dfd7c2f7566d46bfeeed85beab8
SzalontaiKrisztian <krisztian.szalontai@outlook.com>
2018-05-31 14:40:44 +0200

    Anagram Class and the functions ready

---
+++ week-04/day-4/Anagram/src/Anagram.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Anagram {
+//  Write a function, that takes two strings and returns a boolean value based on if the two strings are Anagramms or not.
+//  Create a test for that.
+  public static boolean isAnagram(String firstText, String secondText) {
+    ArrayList<String> compareOne = new ArrayList<>(Arrays.asList(firstText.split("")));
+    ArrayList<String> compareTwo = new ArrayList<>(Arrays.asList(secondText.split("")));
+
+    compareOne.sort(String::compareTo);
+    compareTwo.sort(String::compareTo);
+
+
+    if (compareOne.toString().equals(compareTwo.toString())) {
+      System.out.println(compareOne.toString());
+      System.out.println(compareTwo.toString());
+      System.out.println("true");
+      return true;
+    }
+    System.out.println("false");
+    return false;
+  }
+  public static void main(String[] args) {
+
+    Anagram.isAnagram("cinema","iceman");
+
+  }
+}
