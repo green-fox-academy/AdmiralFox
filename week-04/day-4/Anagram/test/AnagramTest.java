75ca39bbc552429da62e71d2b1e1695cfb4cd646
SzalontaiKrisztian <krisztian.szalontai@outlook.com>
2018-05-31 16:36:39 +0200

    Started Anagram test file

---
+++ week-04/day-4/Anagram/test/AnagramTest.java
@@ -0,0 +1,18 @@
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AnagramTest {
+
+  @Test
+  public void isAnagramTest() {
+    String testTextOne = "cinema";
+    String testTextTwo = "iceman";
+
+
+
+
+
+  }
+}
