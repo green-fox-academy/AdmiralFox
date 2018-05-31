401886784d6207c247bc5dc6e8a749f436340a18
SzalontaiKrisztian <krisztian.szalontai@outlook.com>
2018-05-31 17:04:08 +0200

    AnagramTest new Uppercase text

--- week-04/day-4/Anagram/test/AnagramTest.java
+++ week-04/day-4/Anagram/test/AnagramTest.java
@@ -6,13 +6,20 @@ import static org.junit.Assert.*;
 public class AnagramTest {
 
   @Test
-  public void isAnagramTest() {
+  public void anagram_Test_with_two_strings() {
+    Anagram text = new Anagram();
     String testTextOne = "cinema";
     String testTextTwo = "iceman";
 
-
-
-
-
+    assertTrue(text.isAnagram(testTextOne,testTextTwo));
   }
+  @Test
+  public void anagram_Test_with_Uppercase() {
+    Anagram text = new Anagram();
+    String testTextOne = "cinema";
+    String testTextTwo = "iceman";
+
+    assertTrue(text.isAnagram(testTextOne,testTextTwo));
+
+
 }
