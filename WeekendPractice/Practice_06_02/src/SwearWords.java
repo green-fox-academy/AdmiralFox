64f16b7b099b7cf8a364ed9be33a6ba6fbaa1415
SzalontaiKrisztian <krisztian.szalontai@outlook.com>
2018-06-02 16:24:22 +0200

    SwearWords function stuck with solution

--- WeekendPractice/Practice_06_02/src/SwearWords.java
+++ WeekendPractice/Practice_06_02/src/SwearWords.java
@@ -13,17 +13,35 @@ public class SwearWords {
     // Create a function named familyFriendlizer that takes a filename and a list of strings as parameters
     // and remove all the given words from the file and returns the amount of the removed words.
 
-    ArrayList<String> offensiveWords = new ArrayList<> (Arrays.asList("fuck", "bloody", "cock", "shit", "fucker", "fuckstick", "asshole", "dick", "piss"));
-    System.out.println(familyFriendlizer("content.txt", offensiveWords)); // should print out 17
+    ArrayList<String> offensiveWords = new ArrayList<>(Arrays.asList("fuck", "bloody", "cock", "shit", "fucker", "fuckstick", "asshole", "dick", "piss"));
+//    System.out.println(familyFriendlizer("content.txt", offensiveWords)); // should print out 17
+    System.out.println(justPrint("content.txt", offensiveWords));
   }
 
-  private static List<String> familyFriendlizer(String fileName, ArrayList<String> offensiveWords) {
+//  private static int familyFriendlizer(String fileName, ArrayList<String> offensiveWords) {
+//    Path filePath = Paths.get(fileName);
+//    List<String> fileContent = new ArrayList<>();
+//
+//    try {
+//      fileContent = Files.readAllLines(filePath);
+//
+//
+//
+//    } catch (IOException exception) {
+//      System.out.println("Unable to read file");
+//    }
+//    return fileContent.size();
+//  }
+
+  private static List<String> justPrint(String fileName, ArrayList<String> offensiveWords) {
     Path filePath = Paths.get(fileName);
     List<String> fileContent = new ArrayList<>();
-    offensiveWords = new ArrayList<>();
 
     try {
       fileContent = Files.readAllLines(filePath);
+      for (int i = 0; i < fileContent.size(); i++) {
+        System.out.println(fileContent.get(i).split(" "));
+      }
       fileContent.removeAll(offensiveWords);
 
 
@@ -33,3 +51,6 @@ public class SwearWords {
     return fileContent;
   }
 }
+
+
+
