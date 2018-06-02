3c66b5b41d915c79a3060b10a52d96cdc4e98c0c
SzalontaiKrisztian <krisztian.szalontai@outlook.com>
2018-06-02 17:27:12 +0200

    init AnimalShelter.java

---
+++ WeekendPractice/Practice_06_02_Second/src/AnimalShelter.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AnimalShelter {
+
+  int budget;
+  List<String> animals;
+  List<String> adopters;
+
+  public int rescue(Object animal) {
+    animals = new ArrayList<>();
+
+
+  }
+
+}
+
