d22880edee73227ff22727f115e2a25c341ba466
SzalontaiKrisztian <krisztian.szalontai@outlook.com>
2018-06-05 14:06:48 +0200

    practice FileOpen.java

--- WeekendPractice/Practice_06_02/src/FileOpen.java
+++ WeekendPractice/Practice_06_02/src/FileOpen.java
@@ -3,25 +3,30 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+
 public class FileOpen {
-
   public static void main(String[] args) {
-
     Path path = Paths.get("C:\\Users\\krisz\\Greenfox\\AdmiralFox\\WeekendPractice\\Practice_06_02\\content.txt");
-    List<String> fileContetent = new ArrayList<>();
-    String[] content = new String[fileContetent.size()];
+    ArrayList<String> offensiveWords = new ArrayList<> (Arrays.asList("fuck", "bloody", "cock", "shit", "fucker",
+            "fuckstick", "asshole", "dick", "piss"));
 
-    System.out.println(content);
 
-    try {
-      fileContetent = Files.readAllLines(path);
+        try {
+          List<String> content = Files.readAllLines(path);
+          for (int i = 0; i < content.size(); i++) {
+            String[] tomb = content.get(i).split(" ");
+            for (int j = 0; j < tomb.length; j++) {
+              System.out.print(tomb[j] + "/");
+            }
+            content.removeAll(offensiveWords);
+          }
+          int howManyWordsRemoved = content.size() - offensiveWords.size();
+          System.out.println(howManyWordsRemoved);
     } catch (IOException e) {
-      System.out.println("Can not read file!");
-    }
-    for (int i = 0; i <fileContetent.size() ; i++) {
-      content = fileContetent.get(i).split(" ");
-    }
+          e.printStackTrace();
+        }
   }
 }
