35d521d70d0f89fa32945b77fe70ecf568639561
SzalontaiKrisztian <krisztian.szalontai@outlook.com>
2018-06-06 15:12:51 +0200

    refactor(Birds) HashMap function

--- WeekendPractice/Practice_06_02/src/Births.java
+++ WeekendPractice/Practice_06_02/src/Births.java
@@ -10,11 +10,10 @@ public class Births {
   public static void main(String[] args) {
     String fileName = "C:\\Users\\krisz\\Greenfox\\AdmiralFox\\WeekendPractice\\Practice_06_02\\births.csv";
     System.out.println(findBiggestValue(getAges(readFileContent(fileName))));
-
   }
 
-  public static List<String> readFileContent(String filePath) {
-    List<String> fileRows = new ArrayList<>();
+  public static List <String> readFileContent(String filePath) {
+    List <String> fileRows = new ArrayList <>();
     try {
       fileRows = Files.readAllLines(Paths.get(filePath));
     } catch (IOException e) {
@@ -23,28 +22,28 @@ public class Births {
     return fileRows;
   }
 
-  public static List<Integer> getAges(List<String> fileContent) {
-    List<Integer> ages = new ArrayList<>();
+  public static List <Integer> getAges(List <String> fileContent) {
+    List <Integer> ages = new ArrayList <>();
     for (String row : fileContent) {
       ages.add(Integer.parseInt(row.split("[;\\-]")[1]));
     }
     return ages;
   }
 
-  private static HashMap<Integer,Integer> transformToHashMap(List<Integer> ages) {
-    HashMap<Integer,Integer> birthDateCounter = new HashMap<>();
+  private static HashMap <Integer, Integer> transformToHashMap(List <Integer> ages) {
+    HashMap <Integer, Integer> birthDateCounter = new HashMap <>();
     for (int yearDate : ages) {
-      birthDateCounter.merge(yearDate,1, Integer::sum);
+      birthDateCounter.merge(yearDate, 1, Integer::sum);
     }
     return birthDateCounter;
   }
 
 
-  public static Integer findBiggestValue(List<Integer> ages) {
+  public static Integer findBiggestValue(List <Integer> ages) {
     int currentMax = 0;
     int currentKey = 0;
     for (Integer key : ages) {
-      if ( transformToHashMap(ages).get(key) > currentMax) {
+      if (transformToHashMap(ages).get(key) > currentMax) {
         currentMax = transformToHashMap(ages).get(key);
         currentKey = key;
       }
