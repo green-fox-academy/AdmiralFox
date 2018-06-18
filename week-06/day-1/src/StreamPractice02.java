effff1bd55c8704c0e3ab42e2e4c2f6e1d1715f3
SzalontaiKrisztian <krisztian.szalontai@outlook.com>
2018-06-18 13:23:34 +0200

    feat(StreamPractice02) getUppercase from a string task solved

--- week-06/day-1/src/StreamPractice02.java
+++ week-06/day-1/src/StreamPractice02.java
@@ -1,23 +1,30 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class StreamPractice02 {
 
   public static void main(String[] args) {
 
     ArrayList<Integer> numbers = new ArrayList <>(Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14));
+    ArrayList<Integer> numbers2 = new ArrayList<>(Arrays.asList(3, 9, 2, 8, 6, 5));
+    String testString = "Cogito Ergo Sum";
 
     getEvens(numbers);
     getAverageOfOdd(numbers);
     getEvensSquared(numbers);
+    getSquaredMoreThan20(numbers2);
+    getUppercase(testString);
 
   }
   private static void getEvens(ArrayList<Integer> numbers) {
+    System.out.println("getEvens: ");
     numbers.stream()
             .filter(n -> n % 2 == 0)
             .forEach(System.out::println);
   }
   private static void getAverageOfOdd(ArrayList<Integer> numbers) {
+    System.out.println("getAverageOfOdd: ");
     numbers.stream()
             .filter(n -> n % 2 != 0)
             .mapToInt(n -> n)
@@ -25,9 +32,24 @@ public class StreamPractice02 {
             .ifPresent(System.out::println);
   }
   private static void getEvensSquared(ArrayList<Integer> numbers) {
+    System.out.println("getEvensSquared: ");
     numbers.stream()
             .filter(n -> n % 2 == 0)
             .map(n -> n * n)
+            .forEach(System.out::println);
+  }
+  private static void getSquaredMoreThan20(ArrayList<Integer> numbers) {
+    System.out.println("getSquaredMoreThan20: ");
+    numbers.stream()
+            .map(n -> n * n)
+            .filter(n -> n > 20)
+            .forEach(System.out::println);
+  }
+  private static void getUppercase(String testString) {
+    System.out.println("getUppercase: ");
+    testString.chars()
+            .filter(Character::isUpperCase)
+            .mapToObj(c -> Character.toString((char)c))
             .forEach(System.out::println);
   }
 }
