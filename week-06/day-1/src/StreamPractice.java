68903bce60b1b09e2668c393892e0a93ad8f4d37
SzalontaiKrisztian <krisztian.szalontai@outlook.com>
2018-06-18 11:39:29 +0200

    init(StreamPractice) class

---
+++ week-06/day-1/src/StreamPractice.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class StreamPractice {
+
+  ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14));
+
+}
