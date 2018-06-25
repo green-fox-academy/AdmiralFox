65bfd375080f1a9a4c0ec49fffbdf4dcab5c9623
SzalontaiKrisztian <krisztian.szalontai@outlook.com>
2018-06-25 13:48:00 +0200

    init(HelloRESTController): making new controller class as the task requsted

---
+++ week-07/day-1/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/Greeting.java
@@ -0,0 +1,23 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class Greeting {
+
+  long id;
+  String content;
+
+  public Greeting(long id, String content) {
+    this.id = id;
+    this.content = content;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public String getContent() {
+    return content;
+  }
+}
