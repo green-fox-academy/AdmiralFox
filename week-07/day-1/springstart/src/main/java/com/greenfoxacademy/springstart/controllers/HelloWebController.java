c2d53c7e8c480708be1083473c26d8d8d5e848b5
SzalontaiKrisztian <krisztian.szalontai@outlook.com>
2018-06-25 16:08:14 +0200

    feat(HelloWebController): deferent hello words added and used in the app

--- week-07/day-1/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-07/day-1/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -1,16 +1,33 @@
 package com.greenfoxacademy.springstart.controllers;
 
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@RestController
+import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Controller
 public class HelloWebController {
-  
+  private final AtomicLong counter = new AtomicLong();
+  String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+          "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+          "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+          "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
+
   @RequestMapping("/web/greeting")
-  public String greeting(Model model) {
-    model.addAttribute("name", " World");
+  public String greeting(Model model, @RequestParam(value = "name") String name) {
+    model.addAttribute("name", name);
+    model.addAttribute("counter", counter.incrementAndGet());
+    model.addAttribute("hellos", iterateElements(hellos));
     return "greeting";
+  }
+
+  public static String iterateElements(String[] elements) {
+    String random = (elements[new Random().nextInt(elements.length)]);
+    return random;
   }
 }
