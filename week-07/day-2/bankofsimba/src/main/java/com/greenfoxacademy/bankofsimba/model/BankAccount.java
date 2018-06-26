a22827e219cdb70923cd50010de7f9aeda976776
SzalontaiKrisztian <krisztian.szalontai@outlook.com>
2018-06-26 17:04:01 +0200

    refactor(BankAccount): new constructor field added'

--- week-07/day-2/bankofsimba/src/main/java/com/greenfoxacademy/bankofsimba/model/BankAccount.java
+++ week-07/day-2/bankofsimba/src/main/java/com/greenfoxacademy/bankofsimba/model/BankAccount.java
@@ -2,19 +2,40 @@ package com.greenfoxacademy.bankofsimba.model;
 
 public class BankAccount {
 
-  String name;
-  double balance;
-  String showBalance;
-  String animalType;
+  private String name;
+  private double balance;
+  private String showBalance;
+  private String animalType;
+  private boolean isKing;
+  private boolean isGood;
+
 
   public BankAccount() {
   }
 
-  public BankAccount(String name, double balance, String animalType) {
+  public BankAccount(String name, double balance, String animalType, boolean isKing, boolean isGood) {
     this.name = name;
     this.balance = balance;
     showBalance= String.format("%.2f Zebra", balance);
     this.animalType = animalType;
+    this.isKing = isKing;
+    this.isGood = isGood;
+  }
+
+  public boolean isGood() {
+    return isGood;
+  }
+
+  public void setGood(boolean good) {
+    isGood = good;
+  }
+
+  public boolean isKing() {
+    return isKing;
+  }
+
+  public void setKing(boolean king) {
+    isKing = king;
   }
 
   public String getShowBalance() {
@@ -48,4 +69,5 @@ public class BankAccount {
   public void setAnimalType(String animalType) {
     this.animalType = animalType;
   }
+
 }
