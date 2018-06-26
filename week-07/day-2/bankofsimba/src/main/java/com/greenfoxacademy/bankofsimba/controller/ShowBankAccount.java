a4aaf9746b2bd2ca891eb3ae100774f0d4b90e05
SzalontaiKrisztian <krisztian.szalontai@outlook.com>
2018-06-26 14:06:33 +0200

    feat(ShowBankAccount): endpoint added text test

--- week-07/day-2/bankofsimba/src/main/java/com/greenfoxacademy/bankofsimba/controller/ShowBankAccount.java
+++ week-07/day-2/bankofsimba/src/main/java/com/greenfoxacademy/bankofsimba/controller/ShowBankAccount.java
@@ -10,8 +10,13 @@ public class ShowBankAccount {
 
   @RequestMapping("/show")
   public String showRegistrationPage(Model model) {
-    model.addAttribute("data", new BankAccount("Simba", 2000, "lion"));
+    model.addAttribute("data", new BankAccount("Simba", 2000.00, "lion"));
     return "show";
   }
 
+  @RequestMapping("/texttest")
+  public String showTextTest(Model model) {
+    model.addAttribute("testText", "This is an <em>HTML</em> text. <b>Enjoy yourself!</b>");
+    return "texttest";
+  }
 }
