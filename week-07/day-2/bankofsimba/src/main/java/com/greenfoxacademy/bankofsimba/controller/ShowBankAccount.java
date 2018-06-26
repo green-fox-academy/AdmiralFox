d243c2f90e351d7518ef3763f10e04355ccda3ba
SzalontaiKrisztian <krisztian.szalontai@outlook.com>
2018-06-26 17:02:53 +0200

    refactor(ShowBankAccount): new constructor lines filed'

--- week-07/day-2/bankofsimba/src/main/java/com/greenfoxacademy/bankofsimba/controller/ShowBankAccount.java
+++ week-07/day-2/bankofsimba/src/main/java/com/greenfoxacademy/bankofsimba/controller/ShowBankAccount.java
@@ -20,7 +20,7 @@ public class ShowBankAccount {
 
   @RequestMapping("/show")
   public String showRegistrationPage(Model model) {
-    model.addAttribute("data", new BankAccount("Simba", 2000.00, "lion"));
+    model.addAttribute("data", new BankAccount("Simba", 2000.00, "lion", false, false));
     return "show";
   }
 
@@ -32,11 +32,11 @@ public class ShowBankAccount {
   @RequestMapping("/listaccounts")
   public String listAccounts(Model model) {
     model.addAttribute("newBankAccount", bankAccounts);
-    bankAccounts.add(new BankAccount("Zordon",4000.00, "lion"));
-    bankAccounts.add(new BankAccount("Mufasa", 5000.00, "lion"));
-    bankAccounts.add(new BankAccount("Timon", 1000.00, "suricata"));
-    bankAccounts.add(new BankAccount("Zazu",150.00, "tucan"));
-    bankAccounts.add(new BankAccount("Balu", 50000.00, "bear"));
+    bankAccounts.add(new BankAccount("Zordon",4000.00, "lion", false, true));
+    bankAccounts.add(new BankAccount("Mufasa", 5000.00, "lion", true, true));
+    bankAccounts.add(new BankAccount("Timon", 1000.00, "suricata", false ,false));
+    bankAccounts.add(new BankAccount("Zazu",150.00, "tucan", false, true));
+    bankAccounts.add(new BankAccount("Balu", 50000.00, "bear", false, false));
     return "listaccounts";
   }
 }
