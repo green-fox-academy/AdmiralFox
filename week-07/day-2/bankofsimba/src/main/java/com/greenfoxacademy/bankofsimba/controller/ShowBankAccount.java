ab57fbeb9603f0381b87ead65eb091a0c7a23bcb
SzalontaiKrisztian <krisztian.szalontai@outlook.com>
2018-06-26 14:54:30 +0200

    endPoint(ShowBankAccount): added and listed new bank accounts

--- week-07/day-2/bankofsimba/src/main/java/com/greenfoxacademy/bankofsimba/controller/ShowBankAccount.java
+++ week-07/day-2/bankofsimba/src/main/java/com/greenfoxacademy/bankofsimba/controller/ShowBankAccount.java
@@ -5,8 +5,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class ShowBankAccount {
+
+  private List<BankAccount> bankAccounts;
+
+  public ShowBankAccount() {
+    bankAccounts = new ArrayList <>();
+  }
+
 
   @RequestMapping("/show")
   public String showRegistrationPage(Model model) {
@@ -18,5 +28,15 @@ public class ShowBankAccount {
   public String showTextTest(Model model) {
     model.addAttribute("testText", "This is an <em>HTML</em> text. <b>Enjoy yourself!</b>");
     return "texttest";
+  }
+  @RequestMapping("/listaccounts")
+  public String listAccounts(Model model) {
+    model.addAttribute("newBankAccount", bankAccounts);
+    bankAccounts.add(new BankAccount("Zordon",4000.00, "lion"));
+    bankAccounts.add(new BankAccount("Mufasa", 5000.00, "lion"));
+    bankAccounts.add(new BankAccount("Timon", 1000.00, "suricata"));
+    bankAccounts.add(new BankAccount("Zazu",150.00, "tucan"));
+    bankAccounts.add(new BankAccount("Balu", 50000.00, "bear"));
+    return "listaccounts";
   }
 }
