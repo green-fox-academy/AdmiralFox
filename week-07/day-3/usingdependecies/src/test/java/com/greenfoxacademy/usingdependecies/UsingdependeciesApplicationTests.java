4ab579ac50e12f3ddb5a2728f3d6cc05a66772b3
SzalontaiKrisztian <krisztian.szalontai@outlook.com>
2018-06-28 12:46:19 +0200

    gradle libary and stuff added

---
+++ week-07/day-3/usingdependecies/src/test/java/com/greenfoxacademy/usingdependecies/UsingdependeciesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.greenfoxacademy.usingdependecies;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UsingdependeciesApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
---
+++ week-07/day-4/programer-fox-club/src/main/java/com/greenfoxacademy/programerfoxclub/ProgramerFoxClubApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.programerfoxclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramerFoxClubApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ProgramerFoxClubApplication.class, args);
+  }
+}
