9cf5dc25b256324d058621864ed37a0023e20f81
SzalontaiKrisztian <krisztian.szalontai@outlook.com>
2018-06-28 14:57:56 +0200

    feat(ProgrammerFoxClub): GET request handling

--- week-07/day-4/programer-fox-club/src/main/java/com/greenfoxacademy/programerfoxclub/controllers/MainController.java
+++ week-07/day-4/programer-fox-club/src/main/java/com/greenfoxacademy/programerfoxclub/controllers/MainController.java
@@ -1,21 +1,27 @@
 package com.greenfoxacademy.programerfoxclub.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class MainController {
 
   @GetMapping("")
-  public String showMainPage() {
+  public String showMainPage(@RequestParam(value = "name", required = false) String userInput, Model model) {
+    model.addAttribute("name", userInput);
     return "index";
   }
 
-  @PostMapping("/login")
-  public String login(@ModelAttribute(value = "username") String username) {
-    userService.login(username);
-    return "redirect:/profile/" + username;
+  @GetMapping("/login")
+  public String showLogin() {
+    return "login";
   }
+
+  @GetMapping("/{username}")
+  public String renderProfilePage(@PathVariable(value = "username") String username, Model model) {
+    return "";
+  }
+
+
 }
--- week-07/day-4/programer-fox-club/src/main/java/com/greenfoxacademy/programerfoxclub/models/Fox.java
+++ week-07/day-4/programer-fox-club/src/main/java/com/greenfoxacademy/programerfoxclub/models/Fox.java
@@ -1,9 +1,53 @@
 package com.greenfoxacademy.programerfoxclub.models;
+import java.util.ArrayList;
 
 
 public class Fox {
 
-  private String name,
+  private String name;
+  private ArrayList<String> foods;
+  private ArrayList<String> drinks;
+  private ArrayList<String> tricks;
 
+  public Fox() {
+  }
 
+  public Fox(String name) {
+    this.name = name;
+    this.foods = new ArrayList <>();
+    this.drinks = new ArrayList <>();
+    this.tricks = new ArrayList <>();
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public ArrayList <String> getFoods() {
+    return foods;
+  }
+
+  public void setFoods(ArrayList <String> foods) {
+    this.foods = foods;
+  }
+
+  public ArrayList <String> getDrinks() {
+    return drinks;
+  }
+
+  public void setDrinks(ArrayList <String> drinks) {
+    this.drinks = drinks;
+  }
+
+  public ArrayList <String> getTricks() {
+    return tricks;
+  }
+
+  public void setTricks(ArrayList <String> tricks) {
+    this.tricks = tricks;
+  }
 }
