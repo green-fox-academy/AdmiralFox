8511be8c64be5675664791c5d6a0ff5b18466313
SzalontaiKrisztian <krisztian.szalontai@outlook.com>
2018-07-06 09:44:17 +0200

    init(TheReddit): creating the task Application and database connection

---
+++ week-08/day-5/src/main/java/com/greenfoxacademy/thereddit/TheRedditTaskApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.thereddit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheRedditTaskApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TheRedditTaskApplication.class, args);
+  }
+}
