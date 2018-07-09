42d4d1e1518eee5b837c5e65d36351ffae7be855
SzalontaiKrisztian <krisztian.szalontai@outlook.com>
2018-07-09 13:17:13 +0200

    init(frontend test page) task 2 ready to use

---
+++ week-09/day-1/frontend/src/main/java/com/example/frontend/FrontendTaskApplication.java
@@ -0,0 +1,12 @@
+package com.example.frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendTaskApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FrontendTaskApplication.class, args);
+  }
+}
