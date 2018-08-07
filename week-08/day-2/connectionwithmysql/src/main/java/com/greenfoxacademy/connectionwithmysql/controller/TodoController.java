be12b54d3d1c13eeb8ffad584d3a80631633f098
SzalontaiKrisztian <krisztian.szalontai@outlook.com>
2018-08-07 19:24:08 +0200

    refactoring todo app

--- week-08/day-2/connectionwithmysql/src/main/java/com/greenfoxacademy/connectionwithmysql/controller/TodoController.java
+++ week-08/day-2/connectionwithmysql/src/main/java/com/greenfoxacademy/connectionwithmysql/controller/TodoController.java
@@ -1,13 +1,11 @@
 package com.greenfoxacademy.connectionwithmysql.controller;
 import com.greenfoxacademy.connectionwithmysql.model.Todo;
 import com.greenfoxacademy.connectionwithmysql.repository.TodoRepository;
-import com.greenfoxacademy.connectionwithmysql.services.TodoServices;
+import com.greenfoxacademy.connectionwithmysql.services.TodoServicesImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-
 
 @Controller
 public class TodoController {
@@ -15,19 +13,15 @@ public class TodoController {
   @Autowired
   TodoRepository todoRepository;
   @Autowired
-  TodoServices todoServices;
+  TodoServicesImpl todoServices;
 
-  @GetMapping("/todo")
-  public String todo(){
-    return "todo";
+  @GetMapping("/")
+  public String showMainPage(){
+    return "main";
   }
 
   @GetMapping("/todolist")
-  public String list(@RequestParam(value = "isActive", required = false) boolean isActive, Model model,
-                     @RequestParam(value = "search", required = false) String search) {
-    if (isActive) {
-      model.addAttribute("todos", todoRepository.findAllByDone(false));
-    }
+  public String listTodos(@RequestParam(name = "search", required = false) String search, Model model) {
     if(search != null) {
       model.addAttribute("todos", todoRepository.findAllByTitleContaining(search));
     }
@@ -38,7 +32,7 @@ public class TodoController {
   }
 
   @GetMapping("/add")
-  public String addToDoRender(Model model){
+  public String showAdd(Model model){
     model.addAttribute("newTodo", new Todo());
     return "addnewtodo";
   }
@@ -48,19 +42,20 @@ public class TodoController {
     todoRepository.save(todo);
     return "redirect:/todolist";
   }
-  @GetMapping("/{id}/delete")
+
+  @PostMapping("/{id}/delete")
   public String deleteTodo(@PathVariable(value="id") long id ) {
     todoRepository.deleteById(id);
     return "redirect:/todolist";
   }
 
-  @RequestMapping(value="/{id}/edit", method=RequestMethod.GET)
-  public String editTodoPage(@PathVariable(value="id") long id, Model model) {
+  @GetMapping("/{id}/edit")
+  public String showEdit(@PathVariable(value="id") long id, Model model) {
     model.addAttribute("todo", todoRepository.findById(id).get());
     return "edit";
   }
 
-  @RequestMapping(value="/{id}/edit", method=RequestMethod.POST)
+  @PostMapping("/edit")
   public String editTodo(@ModelAttribute Todo todo) {
     todoRepository.save(todo);
     return "redirect:/todolist";
--- week-08/day-2/connectionwithmysql/src/main/java/com/greenfoxacademy/connectionwithmysql/services/TodoServices.java
+++ week-08/day-2/connectionwithmysql/src/main/java/com/greenfoxacademy/connectionwithmysql/services/TodoServices.java
@@ -1,42 +1,4 @@
 package com.greenfoxacademy.connectionwithmysql.services;
 
-
-import com.greenfoxacademy.connectionwithmysql.model.Todo;
-import com.greenfoxacademy.connectionwithmysql.repository.TodoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Optional;
-
-@Service
-public class TodoServices {
-
-  @Autowired
-  private TodoRepository todoRepository;
-
-  public List<Todo> getAllTopics() {
-    return todoRepository.findAll();
-  }
-
-  public Optional<Todo> getTodo(Long id) {
-    return todoRepository.findById(id);
-  }
-
-  public  void addTodo(Todo todo) {
-    todoRepository.save(todo);
-  }
-
-  public void updateTodo(Long id, String title) {
-    Todo todo = new Todo(title);
-    todoRepository.save(todo);
-
-  }
-  public void deleteTodo(Long id) {
-    todoRepository.deleteById(id);
-  }
-
-  public List<Todo> byTodo(String title) {
-    return todoRepository.findAllByTitleContaining(title);
-  }
+public interface TodoServices {
 }
